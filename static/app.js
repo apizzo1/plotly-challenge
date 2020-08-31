a060f86434eae2ee61595c3d49f8f2393a980742
AmberP <63465031+apizzo1@users.noreply.github.com>
2020-08-30 23:13:21 -0400

    adding event listener/handler

--- static/app.js
+++ static/app.js
@@ -42,7 +42,7 @@ d3.json("../data/samples.json").then((data) => {
     otu_ids.forEach(function(array) {
         var top_ten_otu_ids = array.slice(0,10);
         // adding 'OTU' to the beginning of each array element
-        // https://stackoverflow.com/questions/20498409/adding-text-to-beginning-of-each-array-element
+        // source: https://stackoverflow.com/questions/20498409/adding-text-to-beginning-of-each-array-element
         top_ten_otu_ids = top_ten_otu_ids.map(i => 'OTU ' + i);
         top_otu_ids.push(top_ten_otu_ids);
     })
@@ -57,20 +57,37 @@ d3.json("../data/samples.json").then((data) => {
 
     console.log(top_otu_labels);
 
+    // get index from value in array:
+    //  source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf#:~:text=The%20indexOf()%20method%20returns,if%20it%20is%20not%20present.
+    console.log(names.indexOf('941'));
 
-    // plotting data in horizontal bar chart
-    var trace = {
-        type: 'bar',
-        // Use sample_values as the values for the bar chart.
-        x: top_sample_values[0],
-        // Use otu_ids as the labels for the bar chart.
-        y: top_otu_ids[0],
-        //  Use otu_labels as the hovertext for the chart.
-        text: top_otu_labels[0],
-        orientation: 'h'
+    // event listener for menu option change
+    d3.select("#selDataset").on("change", updateDashboard);
+
+    // event handler function
+    function updateDashboard() {
+        var menu_choice = d3.select("#selDataset");
+        var chosen_subject = menu_choice.property("value");
+        // remove 'option' from output text
+        var option_choice = chosen_subject.substring(6);
+        console.log(option_choice);
+
+        // plotting data in horizontal bar chart
+        var trace = {
+            type: 'bar',
+            // Use sample_values as the values for the bar chart.
+            x: top_sample_values[0],
+            // Use otu_ids as the labels for the bar chart.
+            y: top_otu_ids[0],
+            //  Use otu_labels as the hovertext for the chart.
+            text: top_otu_labels[0],
+            orientation: 'h'
+        }
+
+        var plot_data = [trace];
+
+        Plotly.newPlot("bar", plot_data);
     }
 
-    var plot_data = [trace];
-
-    Plotly.newPlot("bar", plot_data);
+    
 });
