b13eb6ff8807b65692382cde737cbb1cc519b0fa
AmberP <63465031+apizzo1@users.noreply.github.com>
2020-09-03 17:22:09 -0400

    cleaning up app.js

--- app.js
+++ app.js
@@ -204,28 +204,6 @@ d3.json("data/samples.json").then((data) => {
         // Plotting
 
         // plotting data in horizontal bar chart using patient index number
-        // var trace = {
-        //     type: 'bar',
-        //     // Use sample_values as the values for the bar chart.
-        //     x: top_sample_values[patient_chosen],
-        //     // Use otu_ids as the labels for the bar chart.
-        //     y: top_otu_ids[patient_chosen],
-        //     //  Use otu_labels as the hovertext for the chart.
-        //     text: top_otu_labels[patient_chosen],
-        //     orientation: 'h'
-        // };
-        // // format plot with axes labels and title
-        // var layout = {
-        //     title: `Top 10 OTUs found in Test Subject ${option_choice}`,
-        //     xaxis: {title: "Sample Values"},
-        //     // reversing order of y axis
-        //     // source: https://stackoverflow.com/questions/46201532/plotly-js-reversing-the-horizontal-bar-chart-in-plotly
-        //     yaxis: {title: "OTU IDs", autorange: 'reversed'}
-        // };
-
-        // var bar_plot_data = [trace];
-
-        // Plotly.newPlot("bar", bar_plot_data, layout);
 
         // Restyle bar plot with selected test subject data
         var x_bar = top_sample_values[patient_chosen];
@@ -236,35 +214,20 @@ d3.json("data/samples.json").then((data) => {
         Plotly.restyle("bar", "text", [text_bar]);
 
 
+        // // Create a bubble chart that displays each sample.
 
-        
-        // Create a bubble chart that displays each sample.
-        var trace2 = {
-            // Use otu_ids for the x values.
-            x: otu_ids[patient_chosen],
-            // Use sample_values for the y values.
-            y: sample_values_array[patient_chosen],
-            mode: 'markers',
-            // Use sample_values for the marker size.
-            // Use otu_ids for the marker colors.
-            marker: {color: otu_ids[patient_chosen],  size: sample_values_array[patient_chosen]},
-            
-            // Use otu_labels for the text values.
-            text: otu_labels[patient_chosen]
-        }
-
-        var layout_bubble = {
-            title: `All OTUs for Test Subject ${option_choice}`,
-            xaxis: {title: "OTU IDs"},
-            yaxis: {title: "Sample Values"}
-        }
-
-        var bubble_plot_data = [trace2];
-
-        Plotly.newPlot("bubble", bubble_plot_data, layout_bubble);
+        // Restyle bubble plot with selected test subject data
+        var x_bubble = otu_ids[patient_chosen];
+        var y_bubble = sample_values_array[patient_chosen];
+        var marker_bubble = {color: otu_ids[patient_chosen],  size: sample_values_array[patient_chosen]};
+        var text_bubble = otu_labels[patient_chosen];
+        Plotly.restyle("bubble", "x", [x_bubble]);
+        Plotly.restyle("bubble", "y", [y_bubble]);
+        Plotly.restyle("bubble", "marker", [marker_bubble]);
+        Plotly.restyle("bubble", "text", [text_bubble]);
 
         // gauge plot to plot the weekly washing frequency of the individual.
-
+        // get wash frequency 
         console.log(demo_data[0].wfreq);
 
         // Restyle Gauge plot with selected test subject data
